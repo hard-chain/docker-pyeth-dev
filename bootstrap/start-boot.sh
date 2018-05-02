7357f450e5366927d01aa60ab70e768182d649a5
yuyongpeng <yuyongpeng@hotmail.com>
2018-05-02 18:01:36 +0800

    update

--- bootstrap/start-boot.sh
+++ bootstrap/start-boot.sh
@@ -2,4 +2,4 @@ echo "Running filebeat service"
 service filebeat start
 
 echo "Launching bootstrap node"
-/usr/local/bin/pyethapp  -m 0 --password /root/.config/pyethapp/password.txt -l eth.chain:info,eth.chainservice:info,eth.validator:info --log-file /root/log/log.txt run
+/usr/local/bin/pyethapp --unlock 1  -m 0 --password /root/.config/pyethapp/password.txt -l eth.chain:info,eth.chainservice:info,eth.validator:info --log-file /root/log/log.txt run
